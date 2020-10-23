0e6138f217abfea517e4a40bace56bb52c357712
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-10-24 02:12:05 +0700

    Update admin layout with foreach.
    Delete FullName field from Login page.
    
    Set user FullName after Login.

--- Models/User.cs
+++ Models/User.cs
@@ -3,19 +3,22 @@ using System.ComponentModel.DataAnnotations;
 namespace Bloggie.Models {
   public class User {
     public int Id { get; set; }
+
     [DataType(DataType.EmailAddress)]
-    //[Required]
     public string Email { get; set; }
+
     [DataType(DataType.Password)]
-    //[Required]
     public string Password { get; set; }
+
     public string FullName { get; set; }
+
     public bool Active { get; set; } = true;
+
     public UserRole Role { get; set; }
   }
 
   public enum UserRole {
-    Writer,//Role mặc định là writer
+    Writer, // Default role
     Moderator,
     Admin
   }
