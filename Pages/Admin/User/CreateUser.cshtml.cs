3239183d32a671ce6c3cbf6a56d24b56a3ce86e0
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-10-24 01:05:02 +0700

    Fix hiding inherited member warning on CreateUser.cshtml.cs

--- Pages/Admin/User/CreateUser.cshtml.cs
+++ Pages/Admin/User/CreateUser.cshtml.cs
@@ -4,47 +4,47 @@ using Bloggie.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Bloggie.Pages.Admin {
-    public class CreateUserModel : PageModel {
-        private BloggieContext db;
-        public CreateUserModel(BloggieContext db) => this.db = db;
+  public class CreateUserModel : PageModel {
+    private BloggieContext db;
+    public CreateUserModel(BloggieContext db) => this.db = db;
 
-        //Đối tượng dùng để ánh xạ thông tin từ form input
-        [BindProperty]
-        public Bloggie.Models.User User { get; set; } = null;
+    //Đối tượng dùng để ánh xạ thông tin từ form input
+    [BindProperty]
+    public new Bloggie.Models.User User { get; set; } = null;
 
+    //Lời nhắn trạng thái
+    //không bắt buộc có
+    public string statusMsg { get; set; } = "init";
+    public IActionResult OnPost() {
+      //Kiểm tra email
+      //Không phân biệt hoa thường 
+      bool isExist = (db.Users.Where(u => u.Email.Equals(User.Email))).ToList().Any();
+
+      //Input hợp lệ và email chưa tồn tại
+      if (ModelState.IsValid && !isExist) {
+        //Lưu email dưới dạng viết thường 
+        User.Email = User.Email.ToLower();
+
+        //Mã hoá mật khẩu
+        User.Password = Hash.GetHashString(User.Password);
+
+        //Thêm vào bảng Users
+        db.Users.Add(User);
+
+        //Lưu thay đổi 
+        db.SaveChanges();
+
+        //Chuyển hướng đến trang mặc định
+        return Redirect("../../");
+      } else {
+        //Email đã tồn tại => tạo tài khoản thất bại
         //Lời nhắn trạng thái
-        //không bắt buộc có
-        public string statusMsg { get; set; } = "init";
-        public IActionResult OnPost() {
-            //Kiểm tra email
-            //Không phân biệt hoa thường 
-            bool isExist = (db.Users.Where(u => u.Email.Equals(User.Email))).ToList().Any();
-            
-            //Input hợp lệ và email chưa tồn tại
-            if (ModelState.IsValid && !isExist) {
-                //Lưu email dưới dạng viết thường 
-                User.Email=User.Email.ToLower();
+        statusMsg = "Not successful";
 
-                //Mã hoá mật khẩu
-                User.Password = Hash.GetHashString(User.Password);
-                
-                //Thêm vào bảng Users
-                db.Users.Add(User);
-                
-                //Lưu thay đổi 
-                db.SaveChanges();
-                
-                //Chuyển hướng đến trang mặc định
-                return Redirect("../../");
-            } else {
-                //Email đã tồn tại => tạo tài khoản thất bại
-                //Lời nhắn trạng thái
-                statusMsg = "Not successful";
+        //Trả về trang hiện tại( trang tạo tài khoản)
+        return Page();
+      }
 
-                //Trả về trang hiện tại( trang tạo tài khoản)
-                return Page();
-            }
-
-        }
     }
+  }
 }
