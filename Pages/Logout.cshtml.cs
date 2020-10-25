2080340bb0fb72a6855779846254269214a2af94
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-10-25 20:02:36 +0700

    Update Login form.

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -6,23 +6,20 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Bloggie.Pages
-{
-  public class LogoutModel : PageModel
-  {
+namespace Bloggie.Pages {
+  public class LogoutModel : PageModel {
     public string ErrorMessage { get; set; }
-    public string SuccessMessage {get; set; }
-    public async Task<IActionResult> OnGet()
-    {
-        if(!HttpContext.User.Identity.IsAuthenticated)
-            return RedirectToPage("/Index");
-        try{
-            await HttpContext.SignOutAsync();
-            SuccessMessage = "Logout Success!";
-        } catch(Exception error){
-            ErrorMessage = error.Message;
-        } 
-        return Page();
+    public string SuccessMessage { get; set; }
+    public async Task<IActionResult> OnGet() {
+      if (!HttpContext.User.Identity.IsAuthenticated)
+        return RedirectToPage("/Index");
+      try {
+        await HttpContext.SignOutAsync();
+        SuccessMessage = "Logout Success!";
+      } catch (Exception error) {
+        ErrorMessage = error.Message;
+      }
+      return Redirect("/Index");
     }
   }
 }
