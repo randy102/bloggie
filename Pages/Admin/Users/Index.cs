c1c0649ceeafa52ca101e83a5dcf17703a80fad0
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-11-04 02:01:13 +0700

    Update.

--- Pages/Admin/Users/Index.cs
+++ Pages/Admin/Users/Index.cs
@@ -1,56 +1,38 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Bloggie.Utils;
 using Bloggie.Data;
 using Bloggie.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 namespace Bloggie.Pages.Admin {
   public class ListUserModel : PageModel {
     private BloggieContext db;
+
     public ListUserModel(BloggieContext db) => this.db = db;
-    public List<Bloggie.Models.User> Users { get; set; } = null;
 
-    //Trả về danh sách các đối tượng cần quản lý ( không bao gồm admin)
-    public List<Bloggie.Models.User> GetUsers() {
-      return db.Users.Where(u => u.Role != UserRole.Admin).ToList();
-    }
+    public List<Bloggie.Models.User> Users { get; set; }
 
-    //Xử lý yêu cầu hiện thị danh sách 
     public void OnGet() {
-      Users = GetUsers();
+      Users = db.Users.Where(u => u.Role != UserRole.Admin).ToList();
     }
 
-    //Xử lý yêu cầu thay đổi quyền của 1 tài khoản
-    public IActionResult OnPostChangeRole(int userId) {
-      //Tìm kiếm tài khoản dựa trên Id
-      var user = db.Users.Find(userId);
+    public IActionResult OnPostChangeRole(int id) {
+      var user = db.Users.Find(id);
 
-      //Thay đổi quyền
       user.Role = (user.Role == UserRole.Moderator ? UserRole.Writer : UserRole.Moderator);
 
-      //Lưu thay đổi
       db.SaveChanges();
 
-      //Chuyển hướng đến trang hiện thị danh sách
       return Page();
     }
 
-    //Xử lý yêu cầu thay đổi trạng thái của 1 tài khoản
-    public IActionResult OnPostChangeActive(int userId) {
+    public IActionResult OnPostChangeActive(int id) {
+      var user = db.Users.Find(id);
 
-      //Tìm kiếm tài khoản dựa trên Id
-      var user = db.Users.Find(userId);
-
-      //Thay đổi trạng thái( block/ unblock)
       user.Active = (user.Active == true) ? false : true;
 
-      //Lưu thay đổi
       db.SaveChanges();
 
-      //Chuyển hướng đến trang hiện thị danh sách
       return Page();
     }
   }
